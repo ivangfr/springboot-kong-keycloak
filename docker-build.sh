ba419e9b531505e378e67196ae61fc21fc4bdb2c
Ivan Franchin <ivan.franchin@takeaway.com>
2024-10-27 08:06:35 +0100

    Replace Jib with Paketo Buildpacks

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+DOCKER_IMAGE_PREFIX="ivanfranchin"
+APP_NAME="book-service"
+APP_VERSION="1.0.0"
+DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${APP_NAME}:${APP_VERSION}"
+SKIP_TESTS="true"
+
 if [ "$1" = "native" ];
 then
-  ./mvnw clean -Pnative spring-boot:build-image --projects book-service -DskipTests
+  ./mvnw -Pnative clean spring-boot:build-image --projects "$APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
 else
-  ./mvnw clean compile jib:dockerBuild --projects book-service
+  ./mvnw clean spring-boot:build-image --projects "$APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
 fi
+
+exit 0
