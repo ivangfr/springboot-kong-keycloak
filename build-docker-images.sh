dbbf92d97886605c897a5321c8311402e68c829c
Ivan Franchin <ivan.franchin@takeaway.com>
2025-01-14 08:46:09 -0300

    Add back Jib as Paketo Buildpacks is not working with Podman

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -6,11 +6,16 @@ APP_VERSION="1.0.0"
 DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${APP_NAME}:${APP_VERSION}"
 SKIP_TESTS="true"
 
-if [ "$1" = "native" ];
-then
-  ./mvnw -Pnative clean spring-boot:build-image --projects "$APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
-else
-  ./mvnw clean spring-boot:build-image --projects "$APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
-fi
+## -- Paketo Buildpacks
+#if [ "$1" = "native" ];
+#then
+#  ./mvnw -Pnative clean spring-boot:build-image --projects "$APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
+#else
+#  ./mvnw clean spring-boot:build-image --projects "$APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
+#fi
 
-exit 0
+## -- Jib
+./mvnw clean compile jib:dockerBuild \
+  --projects "$APP_NAME" \
+  -DskipTests="$SKIP_TESTS" \
+  -Dimage="$DOCKER_IMAGE_NAME"
